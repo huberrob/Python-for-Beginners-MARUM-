944bd7800df336312ca5b6f41834085ff9ddee22
huberrob <huberrob@users.noreply.github.com>
2020-09-06 15:20:53 +0200

    lessons

---
+++ Day 3/My_module.py
@@ -0,0 +1,12 @@
+# clss Person
+#   def __init__(self, name, age):
+#     self.name = name
+#     self.age = age
+    
+#   # function inside a class - method
+#   def greetings(self):
+#     print("Hello my name is " + self.name)
+
+# # example of a function outside the class
+# def get_square(x):
+#     return x**2
